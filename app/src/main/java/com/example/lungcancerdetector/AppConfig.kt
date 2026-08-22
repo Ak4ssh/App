fdba515b96385a762072811c16d8aab59fdf5e40
Akash <akashtix445@gmail.com>
2026-08-22 13:49:18 +0530

    create app config with constants

---
+++ app/src/main/java/com/example/lungcancerdetector/AppConfig.kt
@@ -0,0 +1,8 @@
+package com.example.lungcancerdetector
+
+object AppConfig {
+    const val VERSION = "1.0.0"
+    const val APP_NAME = "LungScan AI"
+    const val MODEL_NAME = "lung_cancer.tflite"
+    const val IMG_SIZE = 200
+}
